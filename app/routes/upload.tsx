4190b9496494cb84725c4872ebacea68ae35c4ed
Sreerag-03 <nairsreerag2@gmail.com>
2025-12-09 21:05:01 +0530

    Cloud Storage, PDF-Image conversion, Key-value storage and AI feedback

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,11 +1,11 @@
 import {type FormEvent, useState} from 'react'
 import Navbar from "~/components/Navbar";
+import FileUploader from "~/components/FileUploader";
 import {usePuterStore} from "~/lib/puter";
 import {useNavigate} from "react-router";
+import {convertPdfToImage} from "~/lib/pdf2img";
+import {generateUUID} from "~/lib/utils";
 import {prepareInstructions} from "../../constants";
-import FileUploader from '~/components/FileUploader';
-import { convertPdfToImage } from '~/lib/psf2img';
-import { generateUUID } from '~/lib/utils';
 
 const Upload = () => {
     const { auth, isLoading, fs, ai, kv } = usePuterStore();
